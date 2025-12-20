bda6414a143314e4befff0fcad55c1216d569277
bayashi <42190+bayashi@users.noreply.github.com>
2025-12-20 12:53:38 +0000

    Just add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {},
+  },
+  {
+    files: ['*.config.js', 'vite.config.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {},
+  },
+];
+
